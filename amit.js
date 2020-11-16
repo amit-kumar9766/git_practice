e16436ad95f6b11117c8208e17ae996c29c0e89e
amit-kumar9766 <amitbalharakr93@gmail.com>
2020-11-16 13:42:02 +0530

    tosee

--- amit.js
+++ amit.js
@@ -1,6 +1,8 @@
 
-const foo=()=>{
-    console.log('foo')
+var el=document.getElementsByTagName('h1')
+el.onClick=changeText(el)
+
+function changeText(id) {
+    id.innerHTML = "Ooops!";
 }
- 
-foo()
+  
